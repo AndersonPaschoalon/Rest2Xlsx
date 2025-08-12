e1657a35f052bbe982565f7a5d4202e1f8c70693
Anderson Paschoalon <anderson.paschoalon@gmail.com>
2025-08-12 12:19:02 -0300

    testando cmake

---
+++ Sources/git-clone-deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# CPR
+git clone https://github.com/libcpr/cpr.git libs/cpr
+
+# simdjson
+git clone https://github.com/simdjson/simdjson.git libs/simdjson
+
+# libxlsxwriter
+git clone https://github.com/jmcnamara/libxlsxwriter.git libs/libxlsxwriter
+
+
+
+
+
